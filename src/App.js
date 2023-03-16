a857e4f4381b487384e3abb14444068ad325b6f2
Ivan Yaremko <yaremich98@gmail.com>
2023-03-16 08:23:37 +0200

    (62) React Работа с формами

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import './App.css';
 import React, { Component } from 'react';
-import UserSection from './components/UserSection';
-import StopWatch from './components/StopWatch';
-import Ciao from './components/CiaoSection/Ciao';
-
-// function App() {
-//   return <>
-//   <UserSection />
-//   </>
-// }
-
-// export default App;
+import styles from './SignInForm.module';
 
 function App() {
     return <>
-    <Ciao name="Elon" id="1" />
-    <StopWatch />
+    <SignInForm />
     </>
   }
   
